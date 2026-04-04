8f01ce7180e25f724d5481c9f85633eb054ed444
bpickert99 <38917124+bpickert99@users.noreply.github.com>
2026-04-04 12:50:31 +0000

    Fix EmailJS template ID

--- src/firebase.js
+++ src/firebase.js
@@ -24,7 +24,7 @@ export const TEST_MODE = false;
 
 export const EMAILJS_CONFIG = {
   serviceId: "service_bxq3x6r",
-  templateId: "service_bxq3x6r",
+  templateId: "template_h0hio4p",
   publicKey: "gbm7J73MV8nBXHpBZZReC",
 };
 
